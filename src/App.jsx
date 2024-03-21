6f612f1ad17940a1fbb846993af56f3bfc5bf6b2
parshyagk23 <gkprashant52@gmail.com>
2024-03-21 23:16:05 +0530

    Project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
-
+import { Routes, Route } from "react-router-dom"
+import ResumePage from './Pages/ResumePage/ResumePage'
+import ProjectPage from './Pages/ProjectPage/ProjectPage'
+import HomePage from "./Pages/HomePage/HomePage"
 function App() {
 
 
   return (
     <>
-    
+        <Routes>
+          <Route path="/" element={<HomePage/>} ></Route>
+          <Route path="/resume" element={<ResumePage/>} ></Route>
+          <Route path="/project" element={<ProjectPage/>} ></Route>
+        </Routes>    
     </>
   )
 }
--- src/Pages/ProjectPage/ProjectPage.jsx
+++ src/Pages/ProjectPage/ProjectPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Project from '../../Components/Project/Project'
 const ProjectPage = () => {
   return (
-    <div>ProjectPage</div>
+    <>
+      <Project/>
+    </>
   )
 }
 
