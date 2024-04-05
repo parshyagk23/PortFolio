125487f2a0736af56e554ffc5a5e95d09c5d4075
parshyagk23 <gkprashant52@gmail.com>
2024-04-05 19:07:05 +0530

    all fix

--- src/Components/Resume/ResumeSkills.jsx
+++ src/Components/Resume/ResumeSkills.jsx
@@ -5,58 +5,60 @@ const ResumeSkills = ({resume}) => {
     
     const skill =[
       {
-          "name": "HTML",
-          "icon": "./src/assets/icons/html5.png"
+         name: "HTML",
+          icon: "/src/assets/icons/html5.png"
       },
       {
-          "name": "CSS",
-          "icon": "./src/assets/icons/css3.png"
+         name: "CSS",
+          icon: "/src/assets/icons/css3.png"
       },
       {
-          "name": "JavaScript",
-          "icon": "./src/assets/icons/js.png"
+         name: "JavaScript",
+          icon: "/src/assets/icons/js.png"
       },
       {
-          "name": "ReactJS", 
-          "icon": "./src/assets/icons/react.png"
+         name: "ReactJS", 
+          icon: "/src/assets/icons/react.png"
       },
       {
-          "name": "ExpressJS",
-          "icon": "./src/assets/icons/express js.png"
+         name: "ExpressJS",
+          icon: "/src/assets/icons/express js.png"
       },
       {
-          "name": "NodeJS",
-          "icon": "./src/assets/icons/nodejs.png"
-      },
-      
-      {
-          "name": "TailwindCSS",
-          "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/48px-Tailwind_CSS_Logo.png"
+         name: "NodeJS",
+          icon: "/src/assets/icons/nodejs.png"
       },
       
       {
-          "name": "Java",
-          "icon": "/src/assets/icons/java.png"
+         name: "TailwindCSS",
+          icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/48px-Tailwind_CSS_Logo.png"
       },
       
       {
-          "name": "MongoDB",
-          "icon": "/src/assets/icons/mongodb.png"
-      },
-      {
-          "name": "MySQL",
-          "icon": "/src/assets/icons/mysql.png"
+         name: "Java",
+          icon: "/src/assets/icons/java.png"
       },
       
       {
-          "name": "GitHub",
-          "icon": "/src/assets/images/Social icons/github.png"
+         name: "MongoDB",
+          icon: "/src/assets/icons/mongodb.png"
+      },
+      {
+         name: "MySQL",
+          icon:  "/src/assets/icons/mysql.png"
+      },
+      
+      {
+         name: "GitHub",
+          icon: "/src/assets/images/Social icons/github.png"
       }
         
   ]
     
- 
+
     
+    
+   
     return (
    
     <main className={styles.RusumeContainer}>
