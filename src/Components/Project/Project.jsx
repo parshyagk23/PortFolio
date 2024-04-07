28f117546ae677e166172b746a71649a8e22a918
parshyagk23 <gkprashant52@gmail.com>
2024-04-07 12:05:59 +0530

    all fix

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -6,17 +6,12 @@ const Project = ({ project }) => {
     <div className={styles.projectcontainer}>
       <section className={styles.project}>
         <div className={styles.description}>
-          <div style={{ display: "flex", gap: "10px" }}>
+          <div style={{ display: "flex", gap: "10px", margin:" 0 0 20px 0" }}>
             <h1> {project.porjectName} </h1>
-            <a href={project.Link} title="deploy here" target="_blank">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="40"
-                viewBox="0 -960 960 960"
-                width="40"
-              >
-                <path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z" />
-              </svg>
+            
+            <a  className={styles.link} href={project.Link} title="deploy here" target="_blank">
+              <b>.</b>
+              <span>Live</span>
             </a>
           </div>
           <p>{project.description}</p>
--- src/Pages/ProjectPage/ProjectPage.jsx
+++ src/Pages/ProjectPage/ProjectPage.jsx
@@ -4,6 +4,8 @@ import Navbar from "../../Components/Navbar/Navbar";
 import jobfinderImg from "../../assets/images/Project images/jobfinder.png";
 import pocketNoteImg from "../../assets/images/Project images/pocketNotes.png";
 import supperAppImg from "../../assets/images/Project images/supperapp.png";
+import RockPaperScissor from "../../assets/images/Project images/rockpaperscissors.png";
+import Calculator from "../../assets/images/Project images/calculator.png";
 import Contact from "../../Components/Contact/Contact";
 import styles from "../../Components/Project/Project.module.css";
 const ProjectPage = () => {
@@ -28,6 +30,20 @@ const ProjectPage = () => {
       img: supperAppImg,
       porjectName: "SuperApp",
       Link: "https://supperapp-7a46b.web.app/",
+    },
+    {
+      description:
+        "Immerse yourself in the classic game of Rock, Paper, Scissors through this interactive web application. Created using HTML, CSS, and JavaScript, the project delivers an engaging experience with responsive design, visually appealing CSS styling, and dynamic gameplay powered by JavaScript logic. Click to make your selection and witness real-time outcomes, showcasing a seamless integration of front-end technologies for an entertaining digital twist on a timeless game.",
+      img: RockPaperScissor,
+      porjectName: "RockPaperScussor",
+      Link: "https://parshyagk23.github.io/RockPaperScissorss/",
+    },
+    {
+      description:
+        "Project design using HTML, CSS, and JavaScript. This minimalist yet functional tool offers straightforward numeric calculations with a clean design. The responsive layout ensures usability across devices, making it a simple and effective solution for quick math tasks.",
+      img: Calculator,
+      porjectName: "Calculator app",
+      Link: "https://parshyagk23.github.io/Calculator/",
     },
   ];
   return (
